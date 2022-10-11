6dd45245d14efca433a5b2722c5873f5f770219c
Valon <valonidv10@hotmail.com>
2022-10-11 23:04:54 +0200

    Structure fix

--- notes-service-react/src/App.js
+++ notes-service-react/src/App.js
@@ -1,23 +1,13 @@
 
-
 //components
 import Home from './components/Home';
 import DataProvider from './context/DataProvider';
-import './App.css';
-import Login from './Login.jsx';
-import SignUp from './SignUp.jsx';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 
 function App() {
   return (
-    <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/*" element={<Home />}/>
-        </Routes>
-      </Router>
+    <DataProvider>
+      <Home />
+    </DataProvider>
   );
 }
 
