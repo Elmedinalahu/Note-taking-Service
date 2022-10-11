31cf50bf746cfd82b098defcc6897476e3d049a1
artanBojaxhi <artan.bojaxhi@life.gjirafa.com>
2022-10-11 23:56:23 +0200

    Connecting frontend with backend

--- notes-service-react/src/App.js
+++ notes-service-react/src/App.js
@@ -7,10 +7,21 @@ import './App.css';
 import Login from './Login.jsx';
 import SignUp from './SignUp.jsx';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
 
 
 function App() {
+  const [note, setData] = useState("");
+  const getNote = () => {
+  
+  fetch("https://localhost:7259/api/Notes")
+    .then((response) => response.json())
+    .then((data) => {
+      setData(data.title + "..." + data.description);
+    });
+  };
   return (
+    
     <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
