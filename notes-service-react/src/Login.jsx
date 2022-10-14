f5f4de2b61155e0a201c25acfc069999d0dd4845
ariantershnjaku <arian.tershnjaku@life.gjirafa.com>
2022-10-14 18:02:09 +0200

    Added tailwindcss in login and signup form

--- notes-service-react/src/Login.jsx
+++ notes-service-react/src/Login.jsx
@@ -1,36 +1,86 @@
 import './App.css';
 import { useNavigate } from "react-router-dom";
+import logoImg from './logo.jfif'
 
 
 const Login = () =>{
     const navigate = useNavigate();
     return (
-        <div className='container'>
-            
-            <div className='loginimg'></div>
-            <div className="login">
-            <h2>LOGIN</h2>
-            <form method = "Post" action = "">
-                <div class="fields">
-                <span class="username fa fa-user"></span>
-                <input type="Email" placeholder="Email" />
-                </div>
-                <div class="fields">
-                <span class="fa fa-lock"></span>
-                <input type="password" placeholder="Password" maxLength={20} />
-                </div>
-                <button onClick={()=>{
-                    navigate('/home');
-                }} id="continue">Log in</button>
-                <div className='sign'>
-                <span>Don't have an account? </span>
-                <a onClick={()=>{
-                        navigate('/signup');
-                }}>Sign Up</a>
-                </div>
-            </form>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://cdn.iconscout.com/icon/free/png-256/google-keep-3627621-3028847.png"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
             </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-violet-600 focus:ring-violet-500"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+                </label>
+              </div>
+              <div className="text-sm">
+                <a href="SignUp" className="font-medium text-violet-600 hover:text-violet-500">
+                  Not a member? SignUp
+                </a>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-600 py-2 px-4 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                </span>
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     );
 }
 export default Login;
--- notes-service-react/src/SignUp.jsx
+++ notes-service-react/src/SignUp.jsx
@@ -1,41 +1,105 @@
 import './App.css';
 import { useNavigate } from "react-router-dom";
+import logoImg from './logo.jfif'
 
 const SignUp = () => {
     const navigate = useNavigate();
     return (
-        
-        <div className='container'> 
-        <div className='loginimg'></div>
-            <div className="signUp">
-                <h2>SIGN UP</h2>
-                <form method="Post" action="">
-                    <div class="fields">
-                    <span class="fa fa-envelope"></span>
-                    <input type="email
-                    " placeholder="Email" />
-                    </div>
-                    <div class="fields">
-                    <span class="fa fa-lock"></span>
-                    <input type="password" placeholder="New Password" maxLength={20} />
-                    </div>
-                    <div class="fields">
-                    <span class="fa fa-lock"></span>
-                    <input type="password" placeholder="Confirm Password" maxLength={20} />
-                    </div>
-                    <button onClick={() => {
-                        navigate('/home');
-                    }} id="continue">Sign up</button>
-                    <div>
-                        <span>Already have an account? </span>
-                        <a onClick={() => {
-                            navigate('/')
-                        }}>Login</a>
-                    </div>
-                    
-                </form>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://cdn.iconscout.com/icon/free/png-256/google-keep-3627621-3028847.png"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Create your account
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+                <div>
+
+                <label htmlFor="FirstName" className="sr-only">
+                 First Name
+                </label>
+                <input
+                  id="FirstName"
+                  name="FirstName"
+                  type="FirstName"
+                  autoComplete="FirstName"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-sm"
+                  placeholder="First Name"/>
+                  </div>
+                  <div>
+
+              <label htmlFor="LastName" className="sr-only">
+                  Last Name
+                </label>
+                <input
+                  id="LastName"
+                  name="LastName"
+                  type="LastName"
+                  autoComplete="LastName"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-sm"
+                  placeholder="Last Name"/>
+                  </div>
+                  <div>
+
+                 <label htmlFor="email-address" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email-address"
+                  name="email-address"
+                  type="email-address"
+                  autoComplete="email-address"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-sm"
+                  placeholder="Email"/>
+                  </div>
+                  <div>
+
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-violet-500 focus:outline-none focus:ring-violet-500 sm:text-sm"
+                  placeholder="Password"/>
+                  </div>
             </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+              </div>
+              <div className="text-sm">
+                <a href="Login" className="font-medium text-violet-600 hover:text-violet-500">
+                  Already a member? Log in
+                </a>
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-600 py-2 px-4 text-sm font-medium text-white hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                </span>
+                Sign Up
+              </button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+        
+  );
 }
 export default SignUp;
